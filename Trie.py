b24ed5fb7cb3beee5b44e4a691f33c036d60658a
Sai Anirudh <62623048+saianirudh205@users.noreply.github.com>
2022-09-20 02:03:34 +0530

    Update Trie.py

--- Trie.py
+++ Trie.py
@@ -3,6 +3,7 @@ class Trie:
     data=set()
     @staticmethod
     def load():
+        #loading all the cities to a static variable called data
         f=open('cities.txt','r',encoding='UTF-8')
         
         for i in f.read().split('\n'):
@@ -13,10 +14,12 @@ class Trie:
         return Trie().data
     @staticmethod
     def insert(new):
+        #inserting new city to the existing data
         Trie().data.add(new)
         return Trie().data
     @staticmethod
     def delete(city):
+        #delete a city to the existing data
         if city in Trie().data:
             Trie().data.remove(city)
         return Trie().data
